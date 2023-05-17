92426e2716ff9e526c4b2c8b12328c227c541e98
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-05-17 15:08:19 +0100

    make md table

--- mcq_tables.py
+++ mcq_tables.py
@@ -1,8 +1,16 @@
 def make_table(column):
-    """Converts to percentages and makes md table"""
+    """Converts to percentages and prints md table"""
     table_dict = column.value_counts().sort_index().to_dict()
     total = sum(table_dict.values())
+    first_iteration = True
     for k, v in table_dict.items():
-        perc = round(v / total * 100, 2)
-        table_dict[k] = '%.2f' % perc + "%"
-    return table_dict
+        perc = '%.2f' % round(v / total * 100, 2) + "%"
+        if first_iteration:
+            header_str = k
+            count_str = perc
+        else:
+            header_str += ' | ' + k
+            count_str += ' | ' + perc
+        first_iteration = False
+    print(header_str)
+    print(count_str)
