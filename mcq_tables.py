3f3549dd3896d44d2d80fb0c48f6c1fd6c06c04a
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-05-17 15:25:56 +0100

    refactor table code

--- mcq_tables.py
+++ mcq_tables.py
@@ -2,15 +2,12 @@ def make_table(column):
     """Converts to percentages and prints md table"""
     table_dict = column.value_counts().sort_index().to_dict()
     total = sum(table_dict.values())
-    first_iteration = True
+    header_str, between_str, count_str = '', '', ''
     for k, v in table_dict.items():
         perc = '%.2f' % round(v / total * 100, 2) + "%"
-        if first_iteration:
-            header_str = k
-            count_str = perc
-        else:
-            header_str += ' | ' + k
-            count_str += ' | ' + perc
-        first_iteration = False
+        header_str += '| ' + k + ' '
+        between_str += '| --- '
+        count_str += '| ' + perc + ' '
     print(header_str)
+    print(between_str)
     print(count_str)
