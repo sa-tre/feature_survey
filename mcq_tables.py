98f2eee000b935623bafb9961432a6736aba2ecf
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-05-17 14:47:56 +0100

    convert to percentage

--- mcq_tables.py
+++ mcq_tables.py
@@ -1,2 +1,8 @@
 def make_table(column):
-    return column.value_counts().sort_index()
+    """Converts to percentages and makes md table"""
+    table_dict = column.value_counts().sort_index().to_dict()
+    total = sum(table_dict.values())
+    for k, v in table_dict.items():
+        perc = round(v / total * 100, 2)
+        table_dict[k] = '%.2f' % perc + "%"
+    return table_dict
