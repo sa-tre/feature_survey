095c18d13fe88d17e2497ac110be476ad28b0eef
Jim Madge <jmadge@turing.ac.uk>
2023-05-17 11:53:22 +0100

    Add some initial tidying of input

--- analysis.py
+++ analysis.py
@@ -10,4 +10,13 @@ def categories(
     drop_words: Optional[list[str]] = None,
     drop_rows: Optional[list[int]] = None
 ) -> Counter[str, int]:
-    return Counter(chain.from_iterable(split(',| ', item) for item in answers))
+
+    # Split responses into a flat list of words
+    words = chain.from_iterable(split(',| ', item) for item in answers)
+
+    # Drop empty characters
+    words = list(filter(lambda x: x != '', words))
+    # convert to lower case
+    words = [item.lower() for item in list(words)]
+
+    return Counter(words)
