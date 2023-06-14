fc80d2366f6ca6f2424b047d69fbdfe508b48f6b
Jim Madge <jmadge@turing.ac.uk>
2023-06-14 14:24:18 +0100

    Add categories per answer function

--- analysis.py
+++ analysis.py
@@ -13,7 +13,6 @@ def categories(
     keep_words: list[str] = [],
     drop_rows: list[int] = []
 ) -> Counter[str, int]:
-
     # Drop empty entries
     answers = answers.dropna()
 
@@ -67,3 +66,48 @@ def categories(
     df = df.assign(percent=df.apply(lambda x: x/total_responses*100))
 
     return df
+
+
+def categories_per_answer(
+    answers: Series,
+    synonyms: dict[str, list[str]] = {},
+    drop_words: list[str] = [],
+    keep_words: list[str] = [],
+    drop_rows: list[int] = []
+) -> Series:
+    # Fill empty entries
+    answers = answers.fillna('')
+
+    # Replace specified drop rows
+    answers = answers.replace(drop_rows, '')
+
+    # Lower case
+    answers = answers.apply(lambda x: x.lower())
+
+    # Consolidate synonyms per answer
+    for preferred, synonyms_ in synonyms.items():
+        for item in synonyms_:
+            answers = answers.apply(lambda x: sub(item, preferred, x, count=0))
+
+    # Split words around ',', '/', and ' '
+    answers = answers.apply(lambda x: split(',|/| ', x))
+
+    # Strip leading and trailing characters from words
+    answers = answers.apply(lambda x: [item.strip('.,!“”\'() ') for item in x])
+
+    # Drop empty characters
+    answers = answers.apply(lambda x: filter(lambda y: y != '', x))
+
+    # Keep only unique words per answer (to avoid counting a word multiple
+    # times from a single answer)
+    answers = answers.apply(lambda x: set(x))
+
+    # Drop words
+    answers = answers.apply(lambda x: x - set(drop_words))
+
+    # Keep only specified words
+    answers = answers.apply(lambda x: x & set(keep_words))
+
+    answers = answers.apply(lambda x: ';'.join(x))
+
+    return pd.DataFrame(answers)
